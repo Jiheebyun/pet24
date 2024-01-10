eddfed3806a616cad702ccd6ef89a8d2f7e85aab
jhbyun <jhbyun@myhenge.co.kr>
2024-01-10 15:36:15 +0900

    [FEAT]FE-About Page : CSS

--- pet24/src/components/Home/BestBlogList.tsx
+++ pet24/src/components/Home/BestBlogList.tsx
@@ -8,71 +8,72 @@ import { Link } from "react-router-dom";
 
 
 const BlogListsStyle = styled.div`
-    height: 100vh;
+    height: 100%;
     width: 100vw;
     min-width: 1080px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     h1{
-        margin: 20px 0 15px 0;
+        margin: 25px 0 20px 0;
     }
     .blog-lists-wrapper{
-        width: 100vw;
         min-width: 1080px;
-        display: flex;
-        justify-content: space-between;
+        list-style-type: none;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        gap: 40px;
+        margin-left: -80px;
     }
-    .lists-container {
-        margin: 15px;
-        margin-left: 30px;
-        margin-right: 30px;
-        min-width: 300px;
-        width: 350px;
+    .list-content-container{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
         height: 450px;
+        max-width: 320px;
         box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
         transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-        background-color: #fdfdfd;
-        border-radius: 10px;
-        img {
-            min-width: 300px;
-            width: 100%;
-            height: 350px;
-            object-fit: cover;
-            border-radius: 10px;
-        }
-        .lists-content-container{
-            width: 100%;
-            h4{
-                padding: 5px;
-                margin: 0;
-                margin-bottom: 10px;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-                font-size: 17px;
-                font-weight: 800;
-                text-align: center;
-            }
-            p{
-                margin: 0;
-                padding: 3px;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-                word-wrap: break-word;
-                -webkit-line-clamp: 2 ;
-                -webkit-box-orient: vertical;
-            }
-            button{
-                border: none;
-                outline: none;
-                background-color: inherit ;
-                cursor: pointer;
-            }
-        }
-
-        
+        border-radius: 20px; 
     }
+    .list-img-container{ // 이미지 컨텐트 박스 
+        height: 300px;
+        border-radius: 20px 20px 0 0;
 
-
+        img {
+            min-width: 200px;
+            width: 100%;
+            height: 300px;
+            object-fit: cover;
+            border-radius: 20px 20px 0 0;
+            }
+    }
+    .content-container{ //글 컨텐트 박스 
+        width: 100%;
+        height: 150px;
+        border-radius: 0 0 20px 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+   
+        h3{
+         margin: 5px;
+         font-weight: 800;
+        }
+        span{
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            width: 250px;
+            height: 20px;
+        }
+        button{
+            border: none;
+            outline: none;
+            background-color: inherit ;
+            cursor: pointer;
+        }
+    }
 `
 
 const LinkStyle: any ={
@@ -115,6 +116,85 @@ const data = [
     url: "https://www.bbc.co.uk/news/av/world-us-canada-67700100",
     urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/7835/production/_132037703_p0gz6z13.jpg"
     },
+    {
+        author: null,
+        content: "Dashcam footage shows drivers trying to steer the dog towards exits on the Staten Island Expressway in New York. \r\nSeveral cars slowed down to corral the dog to one side, before one motorist gave cha… [+152 chars]",
+        description: "Dashcam footage shows drivers trying to steer the dog towards exits on the Staten Island Expressway.",
+        publishedAt: "2023-12-13T03:06:05Z",
+        source: {id: 'bbc-news', name: 'BBC News'},
+        title: "Watch: Motorists block road to save runaway chihuahua",
+        url: "https://www.bbc.co.uk/news/av/world-us-canada-67700100",
+        urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/7835/production/_132037703_p0gz6z13.jpg"
+    },
+    {
+        author: "https://www.facebook.com/bbcnews",
+        content: "American bully XL dogs in England and Wales must be kept on a lead and muzzled in public now that a new law has come into effect. \r\nBreeding, selling or abandoning them is also illegal as of New Year… [+2413 chars]",
+        description: "Under new rules, owners of the breed must also ensure their dog is muzzled in public.",
+        publishedAt: "2023-12-31T02:17:12Z",
+        title: "American bully XL dogs must now be kept on a lead",
+        url: "https://www.bbc.co.uk/news/uk-67851343",
+        urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/17CCD/production/_131058479_dogs2.jpg",
+        source: {
+            id: "bbc-news",
+            name: "BBC News"
+        }
+        },
+        {
+        author: null,
+        content: "BBC correspondent Sean Dilley has been getting to know his new guide dog, also called Shawn. The pair have been learning to live together - walking, taking public transport, drinking coffee and even … [+384 chars]",
+        description: "Correspondent Sean Dilley has been getting to know his new guide dog, also called Shawn.",
+        publishedAt: "2024-01-05T12:34:41Z",
+        source: {id: 'bbc-news', name: 'BBC News'},
+        title: "BBC reporter Sean Dilley finally meets his new guide dog",
+        url: "https://www.bbc.co.uk/news/av/uk-67890376",
+        urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/2B03/production/_132211011_p0h387r0.jpg"
+        },
+        {
+            author: "https://www.facebook.com/bbcnews",
+            content: "American bully XL dogs in England and Wales must be kept on a lead and muzzled in public now that a new law has come into effect. \r\nBreeding, selling or abandoning them is also illegal as of New Year… [+2413 chars]",
+            description: "Under new rules, owners of the breed must also ensure their dog is muzzled in public.",
+            publishedAt: "2023-12-31T02:17:12Z",
+            title: "American bully XL dogs must now be kept on a lead",
+            url: "https://www.bbc.co.uk/news/uk-67851343",
+            urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/17CCD/production/_131058479_dogs2.jpg",
+            source: {
+                id: "bbc-news",
+                name: "BBC News"
+            }
+            },
+            {
+            author: null,
+            content: "BBC correspondent Sean Dilley has been getting to know his new guide dog, also called Shawn. The pair have been learning to live together - walking, taking public transport, drinking coffee and even … [+384 chars]",
+            description: "Correspondent Sean Dilley has been getting to know his new guide dog, also called Shawn.",
+            publishedAt: "2024-01-05T12:34:41Z",
+            source: {id: 'bbc-news', name: 'BBC News'},
+            title: "BBC reporter Sean Dilley finally meets his new guide dog",
+            url: "https://www.bbc.co.uk/news/av/uk-67890376",
+            urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/2B03/production/_132211011_p0h387r0.jpg"
+            },
+            {
+                author: "https://www.facebook.com/bbcnews",
+                content: "American bully XL dogs in England and Wales must be kept on a lead and muzzled in public now that a new law has come into effect. \r\nBreeding, selling or abandoning them is also illegal as of New Year… [+2413 chars]",
+                description: "Under new rules, owners of the breed must also ensure their dog is muzzled in public.",
+                publishedAt: "2023-12-31T02:17:12Z",
+                title: "American bully XL dogs must now be kept on a lead",
+                url: "https://www.bbc.co.uk/news/uk-67851343",
+                urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/17CCD/production/_131058479_dogs2.jpg",
+                source: {
+                    id: "bbc-news",
+                    name: "BBC News"
+                }
+                },
+                {
+                author: null,
+                content: "BBC correspondent Sean Dilley has been getting to know his new guide dog, also called Shawn. The pair have been learning to live together - walking, taking public transport, drinking coffee and even … [+384 chars]",
+                description: "Correspondent Sean Dilley has been getting to know his new guide dog, also called Shawn.",
+                publishedAt: "2024-01-05T12:34:41Z",
+                source: {id: 'bbc-news', name: 'BBC News'},
+                title: "BBC reporter Sean Dilley finally meets his new guide dog",
+                url: "https://www.bbc.co.uk/news/av/uk-67890376",
+                urlToImage: "https://ichef.bbci.co.uk/news/1024/branded_news/2B03/production/_132211011_p0h387r0.jpg"
+                },
 
 ]
 
@@ -127,26 +207,25 @@ type Props = {
 
 export const BestBlogLists  : React.FC<Props> = ({blogsData}) => {
 
-
-
     return (
         <BlogListsStyle>
                 <h1> Articles</h1>
-                <div className="blog-lists-wrapper">
+                <ul className="blog-lists-wrapper">
                     {data.map((list, idx)=>{
                         return (
-                            <div key ={idx} className="lists-container">
-                                <Link to={""} style={LinkStyle}><img src={list.urlToImage}></img></Link>
-                                <div className="lists-content-container">
-                                    <Link to={""} style={LinkStyle}><h4>{list.title}</h4></Link>
-                                    <Link to={""} style={LinkStyle}><p>{list.content}</p></Link>
+                            <li key={idx} className="list-content-container">
+                                <div className="list-img-container">
+                                    <Link to={""} style={LinkStyle}><img src={list.urlToImage}></img></Link>
+                                </div>
+                                <div className="content-container">
+                                    <Link to={""} style={LinkStyle}><h3>{list.title}</h3></Link>
+                                    <span>{list.content}</span>
                                     <button>view more</button>
                                 </div>
-
-                            </div>
+                            </li>
                         )
                     })}
-                </div>
+                </ul>
         </BlogListsStyle>
     )
 };
--- pet24/src/components/footer.tsx
+++ pet24/src/components/footer.tsx
@@ -6,7 +6,7 @@ import styled from "styled-components";
 
 
 const FooterStyle = styled.footer`
-    width: 100vw;
+    width: 100%;
     height: 100px;
     margin-top: auto;
     background-color: #dde0ea;
