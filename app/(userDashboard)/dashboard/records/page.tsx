141d5929fb8ecfa54f8ad07bb41cd433e7d3527b
Jihee Byun <jh910805jh@gmail.com>
2024-02-12 23:16:21 +0900

    [FEAT] FE: dashboard/records- created table's outline

--- app/(userDashboard)/dashboard/records/page.tsx
+++ app/(userDashboard)/dashboard/records/page.tsx
@@ -19,9 +19,41 @@ export default function Record(){
                         <h2>Records</h2>
                         <h3>88</h3>
                     </div>
-                    <Image></Image>
+                    {/* <Image></Image> */}
                 </div>
                 <div>
+                    <table className={classes.recordsTable} border={1}>
+                        <th>Select</th>
+                        <th>Image</th>
+                        <th>Pet info</th>
+                        <th>Title</th>
+                        <th>Vet Name</th>
+                        <th>Status</th>
+                        <th>Diagnosis Date</th>
+                        <th>Options</th>
+                        <tr>
+                            <td>첫번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                        </tr>
+
+                        <tr>
+                            <td>첫번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                            <td>두번째 칸</td>
+                        </tr>
+                    </table>
+
 
                 </div>
 
