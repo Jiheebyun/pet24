58ad1ac0d34ba744b4064c9c04bb84694c30f0ba
jhbyun <jhbyun@myhenge.co.kr>
2024-02-14 14:13:36 +0900

    [FEAT] Added Next-Auth library

--- app/(userDashboard)/dashboard/profile/page.tsx
+++ app/(userDashboard)/dashboard/profile/page.tsx
@@ -7,7 +7,7 @@ import classes from "./page.module.css";
 export default function Profile (){
     return(
         <>
-            <h1>THIS IS PROFILE</h1>
+            <h2>Your Profile</h2>
         </>
     )
 }
